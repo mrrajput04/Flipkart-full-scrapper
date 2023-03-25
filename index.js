60945ddc21167908ee0b78d5fb509ddbecd02e1b
Divesh Rajput <rajpootdivesh5@gmail.com>
2023-03-25 16:56:23 +0530

    completed

--- index.js
+++ index.js
@@ -22,30 +22,37 @@ async function scrapper(pageNo) {
   if (pageNo === 0) {
     return "completed";
   }
-  const flipkartData = await fetch(
+  const productData = await fetch(
     `https://www.flipkart.com/search?q=tshirt&page=${pageNo}`
   ).catch((err) => console.log("can't fetch data", err));
 
-  const productLink = await parseProductLinks(flipkartData);
-
-  console.log(productLink,'======<<')
+  const productLink = await parseProductLinks(productData);
   for (let i of productLink) {
     const productDom = await fetch(`https://flipkart.com${i}`);
     const data = parseProductDetails(productDom);
     Products.push(data);
   }
-  Products.map((result) => {
+  let query = "";
+  for (const result of Products) {
     try {
-      console.log(result, "===<<");
-      return;
-      // await client.query(`
-      // INSERT INTO TABLE  (result)
-      // `);
-      console.log("data saved");
-    } catch (error) {
-      console.error("can not save data");
+      let des = result.description.replace("'", '"');
+      query = `INSERT INTO Products (title,imageURL,description,price,rating,highlight,colorAndSizeAvailable) VALUES('"${
+        result.title
+      }"','"${result.imageURL}"','${des}','${JSON.stringify(
+        result.price
+      )}','${JSON.stringify(result.rating)}','${JSON.stringify(
+        result.highlight
+      )}','${JSON.stringify(
+        result.colorAndSizeAvailable
+      )}'); `;
+      await client.query(query);
+
+      console.log("data save");
+    } catch (err) {
+      console.log(result.description);
+      console.log("data not save", err);
     }
-  });
+  }
   return scrapper(pageNo - 1);
 }
 
